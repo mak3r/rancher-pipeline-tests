56d662c401af6415c3033e2a7e25d95277915ced
Mark Abrams <mabrams@helpguest.com>
2019-06-24 20:27:47 -0400

    echo vars

---
+++ echo-vars/echo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "${CICD_GIT_REPO_NAME}"
+echo "${CICD_GIT_URL}"
+echo "${CICD_GIT_COMMIT}"
+echo "${CICD_GIT_BRANCH}"
+echo "${CICD_GIT_REF}"
+echo "${CICD_GIT_TAG}"
+echo "${CICD_EVENT}"
+echo "${CICD_PIPELINE_ID}"
+echo "${CICD_EXECUTION_SEQUENCE}"
+echo "${CICD_EXECUTION_ID}"
+echo "${CICD_REGISTRY}"
+echo "${CICD_IMAGE}"
